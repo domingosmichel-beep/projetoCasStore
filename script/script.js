86f17243d4eb078430e4a7cc13a898c2faf6b5c0
michel <domingos.michel@escola.pr.gov.br>
2026-05-04 10:53:34 -0300

    carstore

---
+++ script/script.js
@@ -0,0 +1,10 @@
+fumction salvarCarros(Event) {
+    Event.preventDefault();
+
+    let titulo = document.getElementById('title'). 
+    Value;
+    let titulo = document.getElementById('numero'). 
+    Value;
+    let titulo = document.getElementById('marca'). 
+    Value;
+}
